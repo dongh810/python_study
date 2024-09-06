81b2a5d347c75e6eb3107aa6ef4602f65bd4c343
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-09-07 04:42:38 +0900

    기초문법

---
+++ python/basic.py
@@ -0,0 +1,24 @@
+# 사칙연산
+a=1
+b=2
+print(a+b)
+
+# 조건문
+a=3
+if a>1:
+    print("a is greater than 1")
+
+# 반복문 for
+for a in[1,2,3]:
+    print(a)
+
+# 반복문 while
+i=0
+while i < 3:
+    i=i+1
+    print(i)
+
+# 함수
+def sum(a,b):
+    return a+b
+print(sum(3,4))
